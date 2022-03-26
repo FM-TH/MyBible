115a5d24be217a86a4b5cae2907a59e870ef4f77
tahara <taharafumiya0219@gmail.com>
2022-03-26 21:46:09 +0900

    modelの要素表示

--- mybible_app/models.py
+++ mybible_app/models.py
@@ -3,11 +3,12 @@ import datetime
 
 # Create your models here.
 
+
 class Book(models.Model):
-    book_title = models.CharField(max_length=20)
-    keyword1 = models.CharField(max_length=10)
-    keyword2 = models.CharField(max_length=10,null=True)
-    keyword3 = models.CharField(max_length=10,null=True)
-    comment = models.CharField(max_length=140,null=True)
+    book_title = models.CharField(max_length=50)
+    keyword1 = models.CharField(max_length=50)
+    keyword2 = models.CharField(max_length=50, null=True)
+    keyword3 = models.CharField(max_length=50, null=True)
+    comment = models.CharField(max_length=140, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
