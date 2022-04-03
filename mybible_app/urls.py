7067383be9962959444ad2f8f8108bbf6a1c58c2
tahara <taharafumiya0219@gmail.com>
2022-04-03 10:37:31 +0900

    create

--- mybible_app/urls.py
+++ mybible_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BiblesList, HomeView
+from .views import BiblesList, HomeView, MypageView, BibleCreate
 
 urlpatterns = [
-    path('', HomeView),
-    path('bibles', BiblesList.as_view()),
+    path('', HomeView.as_view(), name='home'),
+    path('mypage/', MypageView.as_view(), name='mypage'),
+    path('bibles/', BiblesList.as_view(), name='list'),
+    path('create/', BibleCreate.as_view(), name='create'),
 ]
--- mybible_app/views.py
+++ mybible_app/views.py
@@ -1,12 +1,27 @@
+from dataclasses import field
 from django.shortcuts import render
-from django.views.generic.list import ListView
+from django.views.generic import ListView, TemplateView, CreateView
 from .models import Book
+from django.urls import reverse_lazy
+
 
 # Create your views here.
 
 
-def HomeView(request):
-    return render(request, "home.html")
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+
+class MypageView(TemplateView):
+    template_name = 'mypage.html'
+
+
+class BibleCreate(CreateView):
+    template_name = 'create.html'
+    model = Book
+    fields = ('book_title', 'author', 'keyword1',
+              'keyword2', 'keyword3', 'comment')
+    success_url = reverse_lazy('mypage')
 
 
 class BiblesList(ListView):
